9fa8e96913884a925ad09f6991453be0c68aef81
PauloCoelho1989 <paulo.coelho@datacomp.pt>
2014-04-11 12:01:32 +0100

    Update table_scripts.sql

--- table_scripts.sql
+++ table_scripts.sql
@@ -4,10 +4,10 @@ CREATE LOGIN [RentMe] WITH PASSWORD = '123456', DEFAULT_DATABASE=[Arrenda_me], D
 
 CREATE TABLE [User]
 (
-  id_User int identity(1,1) NOT NULL PRIMARY KEY,
-  email varchar(50) NOT NULL,
-  pass varchar(20) NOT NULL,
-  contact int,
+	id_User int identity(1,1) NOT NULL PRIMARY KEY,
+	email varchar(50) NOT NULL,
+	pass varchar(20) NOT NULL,
+	contact int,
 );
 
 
@@ -28,18 +28,26 @@ ON Announcement (Price, Longitude, Latitude)
 
  CREATE TABLE Property
 (
-id_District int  NOT NULL,
-id_City int  NOT NULL,
-id_Locality int  NOT NULL, --freguesia
-id_Classification int  NOT NULL,
-active bit  NOT NULL,
-area int NULL,
-title varchar(255)  NOT NULL,
-description varchar(MAX)  NOT NULL,
-id_User int  NOT NULL, 
-FOREIGN KEY (id_User) REFERENCES Users(id_User),
-id_Property int NOT NULL PRIMARY KEY
+	id_District int  NOT NULL,
+	id_City int  NOT NULL,
+	id_Locality int  NOT NULL, --freguesia
+	id_Classification int  NOT NULL,
+	active bit  NOT NULL,
+	area int NULL,
+	title varchar(255)  NOT NULL,
+	description varchar(MAX)  NOT NULL,
+	id_User int  NOT NULL, 
+	FOREIGN KEY (id_User) REFERENCES Users(id_User),
+	id_Property int NOT NULL PRIMARY KEY
 );
+
+CREATE TABLE Typology
+(
+	Id_Typology INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[Type] VARCHAR(3)
+)
+INSERT INTO Typology
+VALUES ('T0'), ('T1'), ('T2'), ('T3'), ('T4'), ('T5'), ('>T5')
 
 CREATE TABLE Attribute
 (
@@ -54,15 +62,13 @@ CREATE TABLE Attribute
 CREATE TABLE [Notification]
 
 (	
-Id_Notification INT NOT NULL PRIMARY KEY,
-Name varchar (50),
-Contact int,
-Email varchar (50),
-Mensage varchar (Max),
-id_User Int,
-id_Announcement int, 
-FOREIGN KEY (id_User) REFERENCES [User](id_User),
-FOREIGN KEY (id_Announcement) REFERENCES [Announcement](id_Announcement),
-
-
+	Id_Notification INT NOT NULL PRIMARY KEY,
+	Name varchar (50),
+	Contact int,
+	Email varchar (50),
+	Mensage varchar (Max),
+	id_User Int,
+	id_Announcement int, 
+	FOREIGN KEY (id_User) REFERENCES [User](id_User),
+	FOREIGN KEY (id_Announcement) REFERENCES [Announcement](id_Announcement),
 )

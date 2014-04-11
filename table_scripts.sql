db1fc936feda9a325cc03f9b94500e2c62fd49ec
PauloCoelho1989 <paulo.coelho@datacomp.pt>
2014-04-11 11:22:25 +0100

    Update table_scripts.sql

--- table_scripts.sql
+++ table_scripts.sql
@@ -1,7 +1,22 @@
- CREATE TABLE Users
+CREATE TABLE Users
 (
-id_User int NOT NULL PRIMARY KEY,
-email varchar(50),
-pass varchar(20),
-contact int,
+  id_User int NOT NULL PRIMARY KEY,
+  email varchar(50),
+  pass varchar(20),
+  contact int,
 );
+
+CREATE TABLE Announcement
+(
+	Id_Announcement INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	Id_Property INT NOT NULL,
+	Price FLOAT NOT NULL,
+	Longitude FLOAT NOT NULL,
+	Latitude FLOAT NOT NULL,
+	StartDate DATETIME NOT NULL,
+	EndDate DATETIME NOT NULL
+)
+CREATE INDEX Announcement_Index
+ON Announcement (Price, Longitude, Latitude)
+
+

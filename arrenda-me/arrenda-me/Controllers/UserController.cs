47b8652084b4b123ecad82516d8b6d342b5bba52
TaniaNovo <tania.coelho19@hotmail.com>
2014-04-15 15:58:22 +0100

    criados modelos

---
+++ arrenda-me/arrenda-me/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace arrenda_me.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Users/
+        public ActionResult Users()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ arrenda-me/arrenda-me/Models/Advertisement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace arrenda_me.Models
+{
+    public class Advertisement
+    {
+        public int id { get; set; }
+        public int idProperty { get; set; }
+        public int price { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public int startDate { get; set; }
+        public int endDate { get; set; }
+
+
+    }
+}
